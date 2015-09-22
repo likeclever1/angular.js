fe7fe9a519a79f43048dac8671409e87aeb5401a
YB <yb@nxc.no>
2015-09-22 17:55:20 +0300

    tutorialpoint

--- js/app.js
+++ js/app.js
@@ -1,28 +1,5 @@
-console.log(angular.version);
-var myApp = angular.module('myApp', ['ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider.when('/question',
-            {
-                templateUrl: 'views/question.html',
-                controller: 'QuestionController'
-            });
+var myApp = angular.module("myApp", []);
 
-        $routeProvider.when('/answer',
-            {
-                templateUrl: 'views/answer.html',
-                controller: 'AnswerController'
-            });
+myApp.controller("MyController", function($scope) {
 
-        $routeProvider.when('/edit/:id',
-            {
-                templateUrl: "views/answer.html",
-                controller: "AnswerController"
-            });
-
-        $routeProvider.when('/edit/:id/:data*',
-            {
-                templateUrl: "views/answer.html",
-                controller: 'AnswerController'
-            });
-        $routeProvider.otherwise({redirectTo: '/question'});
-    });
+});
